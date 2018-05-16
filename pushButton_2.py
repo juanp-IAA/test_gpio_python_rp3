b9d11592a6c5cc468b35e73d3ceb2c68d82a971d
juanp <jpedro@iaa.es>
2018-05-16 19:48:43 +0200

    Ejercicio Boton

--- pushButton_2.py
+++ pushButton_2.py
@@ -4,12 +4,12 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(22, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setmode(GPIO. ) # BCM or BOARD
+GPIO.setup(xx, GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
 print('Press the button.')
 while True:
-     input_state = GPIO.input(22)
+     input_state = GPIO.input(xx)
      if input_state == False:
              print('Button Pressed.')
              time.sleep(0.2)

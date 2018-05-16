2cfb90949aad685e4b313f715f82a24055c8d866
juanp <jpedro@iaa.es>
2018-05-16 19:26:30 +0200

    Hace uso resistor pullup

--- pushButton_2.py
+++ pushButton_2.py
@@ -5,12 +5,12 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(22, GPIO.IN)
+GPIO.setup(22, GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
 print('Press the button.')
 while True:
      input_state = GPIO.input(22)
-     if input_state == True:
-             print('Button Pressed')
+     if input_state == False:
+             print('Button Pressed.')
              time.sleep(0.2)
 
